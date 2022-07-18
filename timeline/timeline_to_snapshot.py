187260e5e76cd59ad42b455d1f14b04f2d143ebf
ZHOU Xiaozhou <617810985@qq.com>
2022-07-18 18:14:27 +0800

    timeline parsing

--- timeline/timeline_to_snapshot.py
+++ timeline/timeline_to_snapshot.py
@@ -3,8 +3,9 @@
 from riot_api import ApiCaller
 import asyncio
 
-import time, re, json
+import time, re, os, json
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
 
@@ -33,6 +34,10 @@ async def getMatchRawData(matchId):
         print(e)
 
 
+def init_champ_status(): # use a ndarray to record
+    result = np.zeros((6, 56)) # 6 item slots, 56d features
+
+
 with open("data/matchIds.json") as fin:
     matchIds = json.load(fin)
 with open("item.json") as fin:
@@ -40,12 +45,26 @@ with open("item.json") as fin:
 all_items = items["data"] # dict: {item_id: {name:, description:, colloq:, ....}, }
 
 
+champion_root = "../src/champ/en_nv/"
+item_root = "../src/item/en/"
 
 loop = asyncio.get_event_loop()
 for id in matchIds:
-    pass
+    tttt = loop.run_until_complete(getTimeline(id)) # json file
+    meta = tttt["meta"] # player, champion, position
+
+    champs = [v[0] for _, v in meta.items()] # 10 participating champions
+    profiles = [pd.read_csv(champion_root + ch.lower() + ".csv").values.tolist() for ch in champs] # for every champ, use a list to record its info
+
+    status = [np.zeros(6, 56) for _ in range(10)] # for 10 champs, each has 6 item slots, feature dim is 56
+    
+    for frame in tttt["timeline"]["frames"]:
+        pass
+
+        
 
 
+loop.close()
 
 
 
